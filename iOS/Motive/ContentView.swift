18f6dcc1cdc4518e734de8df1beda4ca70cbccdc
cemkupeli <63188941+cemkupeli@users.noreply.github.com>
2024-10-16 18:09:33 -0400

    initial commit

---
+++ iOS/Motive/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Motive
+//
+//  Created by Cem Kupeli on 10/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS/Motive/MotiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotiveApp.swift
+//  Motive
+//
+//  Created by Cem Kupeli on 10/15/24.
+//
+
+import SwiftUI
+
+@main
+struct MotiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
