c795882930a7eab9e0bf1d7812f3e39b220cdbc0
cemkupeli <63188941+cemkupeli@users.noreply.github.com>
2024-10-16 18:38:43 -0400

    initial file structure

---
+++ iOS/Motive/Components/CalendarView.swift
@@ -0,0 +1,18 @@
+//
+//  CalendarView.swift
+//  Motive
+//
+//  Created by Cem Kupeli on 10/16/24.
+//
+
+import SwiftUI
+
+struct CalendarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CalendarView()
+}
---
+++ iOS/Motive/Core/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  Motive
+//
+//  Created by Cem Kupeli on 10/16/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
